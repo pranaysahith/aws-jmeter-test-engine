bd2e2fe616798dcc28801747b605e8427561394a
pranaysahith <pranaysahith7@gmail.com>
2020-10-13 13:53:26 +0530

    add readme and add arguments to python script

--- jmeter-icap-poc/cloudformation/create_stack.py
+++ jmeter-icap-poc/cloudformation/create_stack.py
@@ -3,18 +3,34 @@
 
 import boto3
 from math import ceil
+import argparse
 
 
 def main():
 
     client = boto3.client('cloudformation')
 
-    # Update the total users needed for the test
-    total_users = 4000
-    users_per_instance = 4000
+    parser = argparse.ArgumentParser(description='Create cloudformation stack to deploy ASG.')
+    parser.add_argument('--total_users', '-t',
+                        help='total number of users in the test (default: 4000)')
+
+    parser.add_argument('--users_per_instance', '-u',
+                        help='number of users per instance (default: 4000)')
+
+    args = parser.parse_args()
+    
+    if args.total_users:
+        total_users = int(args.total_users)
+    else:
+        total_users = 4000
+
+    if args.users_per_instance:
+        users_per_instance = int(args.users_per_instance)
+    else:
+        users_per_instance = 4000
+    
     # calculate number of instances required
     instances_required = ceil(total_users/users_per_instance)
-
 
     # Load cloudformation template
     with open("GenerateLoadGenerators.json", "r") as f:
@@ -22,6 +38,7 @@ def main():
 
 
     # create ASG with instances to run jmeter tests
+    print("Deploying %s instances in the ASG"%instances_required)
     client.create_stack(
         StackName='aws-jmeter-test-engine-v1-asg',
         TemplateBody=asg_template_body,

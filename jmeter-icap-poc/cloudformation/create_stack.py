7d2e2cd07db2f7c1538ff1431b0553e268a3817f
pranaysahith <pranaysahith7@gmail.com>
2020-10-13 12:33:42 +0530

    update cloudformation template path

--- jmeter-icap-poc/cloudformation/create_stack.py
+++ jmeter-icap-poc/cloudformation/create_stack.py
@@ -9,14 +9,15 @@ def main():
 
     client = boto3.client('cloudformation')
 
-    # calculate number of instances required
+    # Update the total users needed for the test
     total_users = 4000
     users_per_instance = 4000
+    # calculate number of instances required
     instances_required = ceil(total_users/users_per_instance)
 
 
     # Load cloudformation template
-    with open("jmeter-icap-poc/cloudformation/GenerateLoadGenerators.json", "r") as f:
+    with open("GenerateLoadGenerators.json", "r") as f:
         asg_template_body = f.read()
 
 

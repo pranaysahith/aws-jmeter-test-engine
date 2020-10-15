25c1158efefa380b774eb81a6c1e2bcbc56f7b21
pranaysahith <pranaysahith7@gmail.com>
2020-10-15 10:41:57 +0530

    features for issue 33

--- jmeter-icap-poc/cloudformation/README.md
+++ jmeter-icap-poc/cloudformation/README.md
@@ -8,10 +8,22 @@
 pip3 install boto3
 ```
 
-3. Set the AWS profile to be uesd for authentication.
+3. Set AWS authentication.
+
+Get the AWS access key credentails and update in ~/.aws/credentials file for a particular profile. Below is an example of how it should look.
 
 ```
-export AWS_DEFAULT_PROFILE=your_profile
+[glasswall]
+aws_access_key_id = <required value>
+aws_secret_access_key = <required value>
+aws_session_token = <optional value>
+```
+
+Update the ~/.aws/config with the same profile by setting the region you want to use. Below is an example
+
+```
+[profile glasswall]
+region = eu-west-2
 ```
 
 4. Run below command to get help on the script
@@ -26,11 +38,29 @@ Pass `total_users` and `users_per_instance` as input parameters to the script.
 
 `users_per_instance` is the number of users per ec2 instance. Default value of this parameter is 4000
 
-Create the stack by running below command:
+Based on the values passed for total_users and users_per_instance, number of instances required will be calculated. The total users will be equally divided among all the instances.
 
-```
-python3 create_stack.py --total_users 4000 --users_per_instance 4000
+5. Create config file.
+
+Create config.env file by copying config.env.sample file. Update the file with below details:
+
+- aws_profile_name - The AWS profile created in step 3.
+- bucket=gov - Bucket name where a file with number of instances will be created.
+- file_name -  File name with number of instances will be created in the s3 bucket.
+
+6. Create the cloudformation stack by running below command:
+
+Linux or Mac:
+```bash
+export AWS_DEFAULT_PROFILE=your_profile_name
+python create_stack.py --total_users 4000 --users_per_instance 4000
 ```
 
-5. Once the tests are completed, delete the stack from AWS console and create stack again when required.
+Windows powershell or command prompt:
+```powershell
+set AWS_DEFAULT_PROFILE your_profile_name
+python create_stack.py --total_users 4000 --users_per_instance 4000
+```
+
+7. Once the tests are completed, delete the stack from AWS cloudformation using console and run the script again when required.
 
--- jmeter-icap-poc/cloudformation/create_stack.py
+++ jmeter-icap-poc/cloudformation/create_stack.py
@@ -4,11 +4,25 @@
 import boto3
 from math import ceil
 import argparse
+from datetime import datetime
 
 
 def main():
 
-    client = boto3.client('cloudformation')
+    # Load configuration
+    try:
+        with open("config.env") as f:
+            config = f.readlines()
+        configuration = dict(c.strip().split("=") for c in config)
+    except Exception as e:
+        print("Please create config.env file similar to config.env.sample")
+        print(str(e))
+        raise
+
+    # Authenticate to aws
+    profile = configuration.get("aws_profile_name")
+    session = boto3.session.Session(profile_name=profile)
+    client = session.client('cloudformation')
 
     parser = argparse.ArgumentParser(description='Create cloudformation stack to deploy ASG.')
     parser.add_argument('--total_users', '-t',
@@ -31,6 +45,29 @@ def main():
     
     # calculate number of instances required
     instances_required = ceil(total_users/users_per_instance)
+    i = 0
+    while i < 5:
+        if total_users % users_per_instance == 0:
+            instances_required = int(total_users/users_per_instance)
+            break
+        else:
+            if total_users % instances_required == 0:
+                users_per_instance = int(total_users / instances_required)
+            else:
+                instances_required += 1
+        i += 1
+
+    if instances_required * users_per_instance != total_users:
+        print("Please provide total_users in multiples of users_per_instance.")
+        exit(0)
+
+    # write the number of instances required to a file in s3 bucket
+    s3_client = session.client('s3', region_name="us-west-2")
+    bucket = configuration.get("bucket")
+    file_name = configuration.get("file_name")
+    s3_client.put_object(Bucket=bucket,
+                        Body=str(instances_required),
+                        Key=file_name)
 
     # Load cloudformation template
     with open("GenerateLoadGenerators.json", "r") as f:
@@ -38,9 +75,13 @@ def main():
 
 
     # create ASG with instances to run jmeter tests
-    print("Deploying %s instances in the ASG"%instances_required)
+    now = datetime.now()
+    stack_suffix = now.strftime("%Y-%m-%d-%H-%M")
+    stack_name = 'aws-jmeter-test-engine-' + stack_suffix
+    print("Deploying %s instances in the ASG by creating %s cloudformation stack"% (instances_required, stack_name))
+    
     client.create_stack(
-        StackName='aws-jmeter-test-engine-v1-asg',
+        StackName=stack_name,
         TemplateBody=asg_template_body,
         Parameters=[
             {
